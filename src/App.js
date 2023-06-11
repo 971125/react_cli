34d59ca6c67e584d68391d5ff8aff59976344155
971125 <www.1253793386@qq.com>
2023-06-11 20:58:40 +0800

    重置操作

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ function App() {
   const add = () => {
     setCount(() => count + 1)
   }
+  const rest = () => {
+    setCount(0)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Button type='primary' onClick={add}>
           测试button{count}
+        </Button>
+        <Button type='link' onClick={rest}>
+          重置
         </Button>
       </header>
     </div>
